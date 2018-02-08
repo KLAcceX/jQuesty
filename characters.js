cd3094c7b9ce961983be8f17b5322f8730e839e0
KLAcceX <mrrj12@gmail.com>
2018-02-08 15:37:46 -0200

    Basic background

--- characters.js
+++ characters.js
@@ -1,4 +1,4 @@
-const classes = {
+const classe = {
 	criar : function(hp, mp, f, a, v, i, d){
 		return {
 			hp : { atual : hp, max : hp},
@@ -10,12 +10,10 @@ const classes = {
 			destreza : d,
 		};
 	},
-	arqueiro : classes.criar(40, 100, 2, 7, 2, 1, 10),
-	clerigo : classes.criar(70, 100, 2, 1, 7, 10, 2),
-	druida : classes.criar(60, 110, 2, 2, 1, 10, 7),
-	guerreiro : classes.criar(80, 70, 10, 2, 7, 1, 2),
-	ladino : classes.criar(50, 80, 3, 10, 1, 1, 7),
-	mago : clsses.criar(20, 200, 2, 2, 1, 10, 7),
+	arqueiro : classe.criar(40, 100, 2, 7, 2, 1, 10),
+	clerigo : classe.criar(70, 100, 2, 1, 7, 10, 2),
+	druida : classe.criar(60, 110, 2, 2, 1, 10, 7),
+	guerreiro : classe.criar(80, 70, 10, 2, 7, 1, 2),
+	ladino : classe.criar(50, 80, 3, 10, 1, 1, 7),
+	mago : classe.criar(20, 200, 2, 2, 1, 10, 7),
 }
-
-const classes = []
