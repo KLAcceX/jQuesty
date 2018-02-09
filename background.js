48866ee3f382e0c848d3a1fcfb1f83dce974ef7e
KLAcceX <mrrj12@gmail.com>
2018-02-09 14:51:14 -0200

    Editing Background

--- background.js
+++ background.js
@@ -2,16 +2,19 @@ const BACKGROUND = {
   CursorDelay : 200,
   MensagemIntervalo : 50,
   Intervalo : function() {
+    $("#painelIpt").prop('contenteditable', true);
+    $("#painelIpt").focus();
     setInterval(function () {
       if ($('#iptCursor').css('visibility') === 'visible') {
-          $('#iptCursor').css({ visibility: 'hidden' });
+          $('#iptCursor').css({ visibility : 'hidden' });
       } else {
-          $('#iptCursor').css({ visibility: 'visible' });
+          $('#iptCursor').css({ visibility : 'visible' });
       }
     }, BACKGROUND.CursorDelay);
   },
   LimparIntervalo : function() {
-    $('#iptCursor').css({ visibility: 'hidden' });
+    $("#painelIpt").prop('contenteditable', false);
+    $('#iptCursor').css({ visibility : 'hidden' });
     clearInterval(BACKGROUND.Intervalo);
   },
 };
@@ -27,6 +30,7 @@ const PAINEL = {
     }else{
       BACKGROUND.MensagemIntervalo = 50;
       BACKGROUND.Intervalo();
+
     }
   },
   DigitarTexto : function(mensagem){
@@ -36,16 +40,18 @@ const PAINEL = {
   }
 };
 
-$(function(){
-  $("#painelIpt").focus();
-  PAINEL.DigitarTexto("ASDASAAAAAAASPOIQUWELJQWLKJE");
+$(document).on('keydown',function(e){
+  if (e.which === 13) {
+    e.preventDefault();
+    BACKGROUND.MensagemIntervalo = 0;
+  }
+  if($('#painelIpt').width() + 10 > $('#painel').width() && e.which !== 0 && e.keyCode !== 0 &&
+        e.which !== 8 && e.keyCode !== 13 && !e.ctrlKey && !e.metaKey && !e.altKey && !e.shiftKey){
+    e.preventDefault();
+  }
 });
 
-$(document).keypress(function(e) {
-  if (e.which === 13) {
-    BACKGROUND.MensagemIntervalo = 0;
+$(document).bind('mousedown',function(e)
+{
     e.preventDefault();
-    PAINEL.DigitarTexto("AOOOOOOOOOOOOOOOOOOOOOOOOOOOOO");
-    $("#painelIpt").focus();
-  }
 });
---
+++ main.js
@@ -0,0 +1,4 @@
+$(function(){
+	$('#painelIpt').focus();
+  	PAINEL.DigitarTexto("Olá!\nSeja bem-vindo ao mundo de Tcheguenesguever!\n\n");
+})
